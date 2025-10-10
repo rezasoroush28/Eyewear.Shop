aa83f6586bf6f0933ce09e665bb52d745921606c
reza soroush <rezasoroush28@gmail.com>
2025-10-10 14:20:13 +0330

    varient

--- Eyewear.Shop.Domain/Entities/OrderItem.cs
+++ Eyewear.Shop.Domain/Entities/OrderItem.cs
@@ -4,12 +4,10 @@
     {
         public int Id { get; set; }
         public int OrderId { get; set; }
-        public int ProductId { get; set; }
-        public int? ProductVariantId { get; set; }
-        public string ProductName { get; set; } = string.Empty;
-        public decimal UnitPrice { get; set; }
+        public int ProductVariantId { get; set; }
         public int Quantity { get; set; }
 
+        public ProductVariant ProductVarient { get; set; 
         public Order Order { get; set; } = null!;
     }
 
